fbe1d1b5a4a9836fb671bf3169856708a671404f
Barulev Alexandr <huston.mavr@gmail.com>
2017-11-21 14:53:51 +0300

    fixed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -77,6 +77,7 @@ class UserController < ApplicationController
     def index()
         @user = User.new(:name=>'joe', :email=>'em', :password=>'p')
         p user
+        
         render :json => @user
     end
 end
